c760c2e7d10a754755ee206096065bd1394b1a93
jvmartinsdaSilva <jvmartins0855@gmail.com>
2022-06-17 09:36:56 -0300

    att

--- calculator.js
+++ calculator.js
@@ -19,7 +19,7 @@ class Calculator {
         const prevDisplayFloat = parseFloat(this.prevDisplay);
         const resDisplayFloat = parseFloat(this.resDisplay);
 
-        if(isNaN(prevDisplayFloat) || isNaN(resDisplayFloat)) return
+        if(isNaN(prevDisplayFloat) || isNaN(resDisplayFloat)) return;
 
         switch(this.opeation){
             case '+':
@@ -35,73 +35,73 @@ class Calculator {
                 result = prevDisplayFloat / resDisplayFloat
                 break;
             default:
-                return
-        }
+                return;
+        };
 
-        this.resDisplay = result
-        this.opeation = undefined
-        this.prevDisplay = ''
-    }
+        this.resDisplay = result;
+        this.opeation = undefined;
+        this.prevDisplay = '';
+    };
 
     allClear(){
-        this.prevDisplay = ''
-        this.resDisplay = ''
-        this.opeation = undefined
-    }
+        this.prevDisplay = '';
+        this.resDisplay = '';
+        this.opeation = undefined;
+    };
 
     clearLast(){
         this.resDisplay = this.resDisplay.toString().slice(0, -1);
-    }
+    };
 
     attDisplay(){
-        this.prevDisplayText.innerText = `${this.prevDisplay} ${this.opeation || ''}`
-        this.resDisplayText.innerText = `${this.resDisplay}`
-    }
+        this.prevDisplayText.innerText = `${this.prevDisplay} ${this.opeation || ''}`;
+        this.resDisplayText.innerText = `${this.resDisplay}`;
+    };
 
     addNumber(num){
-        if(this.resDisplay.includes('.') && num === '.') return
-        this.resDisplay += num.toString()
-    }
+        if(this.resDisplay.includes('.') && num === '.') return;
+        this.resDisplay += num.toString();
+    };
 
     addOperation(operation){
-        if(this.resDisplay === '') return
+        if(this.resDisplay === '') return;
         if(this.prevDisplay != ''){
-            this.calculate()
-        }
+            this.calculate();
+        };
 
         this.opeation = operation
         this.prevDisplay = `${this.resDisplay}`
         this.resDisplay = ''
-    }
-}
+    };
+};
 
 const calculator = new Calculator(prevDisplayText, resDisplayText)
 
 allClearBTN.addEventListener('click', () => {
     calculator.allClear();
     calculator.attDisplay();
-})
+});
 
 for(const numberBTN of numbersBTN){
     numberBTN.addEventListener('click', () => {
         calculator.addNumber(numberBTN.textContent);
         calculator.attDisplay();
-    })
-}
+    });
+};
 
 for (const operationBTN of operationsBTN){
     operationBTN.addEventListener('click', () => {
         calculator.addOperation(operationBTN.textContent);
         calculator.attDisplay();
-    })
-}
+    });
+};
 
 equalBTN.addEventListener('click', () => {
     calculator.calculate();
     calculator.attDisplay();
-})
+});
 
 clearBTN.addEventListener('click', () => {
     calculator.clearLast();
     calculator.attDisplay();
-})
+});
